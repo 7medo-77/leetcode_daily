dc14883d6b0b4a3731ea49171e6378fd51e0ca77
7medo-77 <130787539+7medo-77@users.noreply.github.com>
2025-02-09 03:41:15 +0200

    [853] [Time Beats: 94.17%] [Memory Beats: 29.64%] - LeetPush

--- 853-Car-Fleet.py
+++ 853-Car-Fleet.py
@@ -1,23 +1,14 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        positionSpeedArray = sorted(list(zip(position, speed)), key=lambda tuple: tuple[0])
+        positionSpeedArray = sorted(list(zip(position, speed)), key=lambda tuple: tuple[0], reverse=True)
         fleetStack = []
-        fleetStack.append(positionSpeedArray[-1])
 
-        for index, tupleElement in enumerate(positionSpeedArray[:-1][::-1]):
+        for index, tupleElement in enumerate(positionSpeedArray):
             carPosition = tupleElement[0]
             carSpeed = tupleElement[1]
             carArrivalTime = (target - carPosition) / carSpeed
 
-            topStackPosition = fleetStack[-1][0]
-            topStackSpeed = fleetStack[-1][1]
-            topStackArrivalTime = (target - topStackPosition) / topStackSpeed
+            if not fleetStack or carArrivalTime > fleetStack[-1]:
+                fleetStack.append(carArrivalTime)
 
-            if (carArrivalTime <= topStackArrivalTime):
-                if (carSpeed <= topStackSpeed):
-                    fleetStack.pop()
-                    fleetStack.append(tupleElement)
-            else:
-                fleetStack.append(tupleElement)
-        print(fleetStack)
         return len(fleetStack)
